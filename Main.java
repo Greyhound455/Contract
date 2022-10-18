3d5d902ac4d996547b5276952298ceef336e7c8b
Greyhound455 <115743145+Greyhound455@users.noreply.github.com>
2022-10-18 13:59:54 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class Main {
+  public static Scanner input = new Scanner(System.in);
+  public static void main(String[] args) {
+
+    System.out.println("\033\143");
+    System.out.println("Sign your name on the Contract and i'll give you a job.");
+    String name = input.next();
+    name = name.substring(0,4);
+    Contract spirit = new Contract(name);
+    System.out.println(spirit.getWord());
+  }
+
+  public static class Contract{
+
+    private String word;
+
+    public Contract(String startWord){
+      word = startWord;
+    }
+
+    public void setWord(String newWord){
+      word = newWord;
+    }
+
+    public String getWord(){
+      System.out.println("Here is your new name: " );
+      return word;
+    }
+  }
+}
